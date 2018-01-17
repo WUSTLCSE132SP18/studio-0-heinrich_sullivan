4b85b4c64d27f77587e9242698d68ecd304c6745
carol <carol@LAPTOP-AN91HVVC.dhcp.wustl.edu>
2018-01-17 15:41:15 -0600

    Sullivan and Heinrich Studio 0

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,19 @@
+int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int secs = millis()/999;
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+  digitalWrite(ledPin, HIGH);
+  Serial.print(secs);
+  Serial.println(" sec have elapsed");
+  Serial.print(millis());
+  Serial.println(" millis elapsed");
+}
